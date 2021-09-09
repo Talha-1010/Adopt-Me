042fa140d570c8434fcb8192b4b50f090fb12b73
Talha-1010 <talhanasr3e@gmail.com>
2021-09-09 15:59:42 +0500

    added searchParams tag

--- src/script.js
+++ src/script.js
@@ -1,13 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
     const App = () =>{
       return(
         <div>
            <h1>Adopt Me!</h1>
-             <Pet name ="Tommy" animal="Dog" breed="german shepherd" />
-             <Pet name ="Milo" animal="Dog" breed="bulldog" />
-             <Pet name ="Jasper" animal="Dog" breed="Siberian Husky" />
+             <SearchParams/>
         </div>
       );
     };
